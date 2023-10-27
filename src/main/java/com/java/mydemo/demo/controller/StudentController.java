bd4755e82dac3689c1da81d96ed2ebc847b6d7da
Ramesh Yadav <ramesh_yadav@tecnics.com>
2023-10-27 13:31:53 -0500

    this is demo project

---
+++ src/main/java/com/java/mydemo/demo/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.java.mydemo.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+	
+	@GetMapping("/welcome")
+	String welcome() {
+		
+		return "Welcome to java and spring boot rest api Application !!!!! ";
+	}
+
+}
